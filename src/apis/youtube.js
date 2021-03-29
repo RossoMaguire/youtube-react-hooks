9c05c0d2e73f36ba479f171e4c3312c18851426b
Ross Maguire <rossmaguire@Rosss-MacBook-Pro.local>
2021-03-29 11:25:31 +0100

    Adding environment variables

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const KEY = "AIzaSyB3SHe3X2UdoB8rY4lSCBdb9VueD9uAe6g";
+const KEY = process.env.REACT_APP_YT_KEY;
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
